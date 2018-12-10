61dd004e52310614a3a7616f89a4cbcf4ccbec44
cornelis <dirk.haupt@gmail.com>
2018-12-10 12:21:49 -0800

    .

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Sloth
     config.load_defaults 5.2
 
     # https://stackoverflow.com/a/19650687
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
