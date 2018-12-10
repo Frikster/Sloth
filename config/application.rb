bfcff66f83ad1b5d3f0eec42a6b63d10c6a9f0fc
cornelis <dirk.haupt@gmail.com>
2018-12-10 12:06:26 -0800

    fix precompile assets failed error on heroku

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Sloth
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # https://stackoverflow.com/a/19650687
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
